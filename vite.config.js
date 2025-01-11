3357f8309176d7d1c0a8d6f53b6fbbe9a2084c8c
Saket Aman <154726958+Saket-Rathor@users.noreply.github.com>
2025-01-12 00:06:43 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-
-    /** If you set esmExternals to true, this plugins assumes that 
-      all external dependencies are ES modules */
- 
     commonjsOptions: {
-       esmExternals: true 
+      esmExternals: true
     },
- }
+  },
+  css: {
+    preprocessorOptions: {
+      sass: {
+        // You can define additional Sass options here if needed
+      }
+    }
+  }
 })
